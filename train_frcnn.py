698d189d0cc00e4b8c594c56c4e4012623f83b4e
Phu Pham <anphu.1225@gmail.com>
2018-01-28 20:14:52 +0200

    refactor

--- train_frcnn.py
+++ train_frcnn.py
@@ -11,6 +11,7 @@ from keras.optimizers import Adam
 from keras.layers import Input
 from keras.models import Model
 # noinspection PyUnboundLocalVariable
+from frcnn import vgg as nn
 from frcnn import config, data_generators
 from frcnn import losses as losses
 from frcnn import focal_loss as focal_loss
@@ -25,20 +26,9 @@ def parse_options():
     parser = OptionParser()
 
     parser.add_option("-p", "--path", dest="train_path", help="Path to training data.")
-    # parser.add_option("-o", "--parser", dest="parser", help="Parser to use. One of simple or pascal_voc",
-    #                   default="pascal_voc")
+
     parser.add_option("-n", "--num_rois", type="int", dest="num_rois", help="Number of RoIs to process at once.",
                       default=32)
-    parser.add_option("--network", dest="network", help="Base network to use. Supports vgg or resnet50.",
-                      default='vgg')
-    parser.add_option("--hf", dest="horizontal_flips",
-                      help="Augment with horizontal flips in training. (Default=false).",
-                      action="store_true", default=False)
-    parser.add_option("--vf", dest="vertical_flips", help="Augment with vertical flips in training. (Default=false).",
-                      action="store_true", default=False)
-    parser.add_option("--rot", "--rot_90", dest="rot_90",
-                      help="Augment with 90 degree rotations in training. (Default=false).",
-                      action="store_true", default=False)
     parser.add_option("--num_epochs", type="int", dest="num_epochs", help="Number of epochs.", default=2000)
     parser.add_option("--config_filename", dest="config_filename", help=
     "Location to store all the metadata related to the training (to be used when testing).",
@@ -46,8 +36,7 @@ def parse_options():
     parser.add_option("--output_weight_path", dest="output_weight_path", help="Output path for weights.",
                       default='./model_frcnn.hdf5')
     parser.add_option("--input_weight_path", dest="input_weight_path",
-                      help="Input path for weights. If not specified, will try to load default weights provided by keras.")
-
+                      help="Path to preload weights. (Default = 'vgg16_weights_tf_dim_ordering_tf_kernels.h5'")
     parser.add_option("--loss_func", dest="loss_func", help="Specify loss function to use. (Default = frcnn)",
                       default='frcnn')
     (options, args) = parser.parse_args()
@@ -62,21 +51,13 @@ def parse_options():
 
     C.model_path = options.output_weight_path
     C.num_rois = int(options.num_rois)
+    # Specify CNN to use: either 'vgg'
+    C.network = 'vgg'
 
     return options, C
 
 
 def init_params(options):
-    # Specify CNN to use: either 'vgg' or 'resnet50'
-    if options.network == 'vgg':
-        C.network = 'vgg'
-        from frcnn import vgg as nn
-    elif options.network == 'resnet50':
-        C.network = 'resnet50'
-        from frcnn import resnet as nn
-    else:
-        print('Not a valid model')
-        raise ValueError
 
     # check if weight path was passed via command line
     if options.input_weight_path:
@@ -145,8 +126,7 @@ def init_params(options):
     except ValueError:
         print("Could not load model weights due to ValueError.")
     except:
-        print(
-            'Could not load pre-trained model weights. Weights can be found in the keras application folder https://github.com/fchollet/keras/tree/master/keras/applications')
+        print('Could not load pre-trained model weights.')
 
     optimizer = Adam(lr=1e-5)
     optimizer_classifier = Adam(lr=1e-5)
@@ -192,12 +172,9 @@ def train(data_gen_train, model_rpn, model_classifier, model_all, num_epochs, ep
                 if len(rpn_accuracy_rpn_monitor) == epoch_length and C.verbose:
                     mean_overlapping_bboxes = float(sum(rpn_accuracy_rpn_monitor)) / len(rpn_accuracy_rpn_monitor)
                     rpn_accuracy_rpn_monitor = []
-                    print(
-                        'Average number of overlapping bounding boxes from RPN = {} for {} previous iterations'.format(
-                            mean_overlapping_bboxes, epoch_length))
+                    print('Average number of overlapping bounding boxes from RPN = {} for {} previous iterations'.format(mean_overlapping_bboxes, epoch_length))
                     if mean_overlapping_bboxes == 0:
-                        print(
-                            'RPN is not producing bounding boxes that overlap the ground truth boxes. Check RPN settings or keep training.')
+                        print('RPN is not producing bounding boxes that overlap the ground truth boxes. Check RPN settings or keep training.')
 
                 X, Y, img_data = next(data_gen_train)
 
@@ -205,8 +182,7 @@ def train(data_gen_train, model_rpn, model_classifier, model_all, num_epochs, ep
 
                 P_rpn = model_rpn.predict_on_batch(X)
 
-                R = roi_helpers.rpn_to_roi(P_rpn[0], P_rpn[1], C, use_regr=True, overlap_thresh=0.7,
-                                           max_boxes=300)
+                R = roi_helpers.rpn_to_roi(P_rpn[0], P_rpn[1], C, use_regr=True, overlap_thresh=0.7, max_boxes=300)
                 # note: calc_iou converts from (x1,y1,x2,y2) to (x,y,w,h) format
                 X2, Y1, Y2, IouS = roi_helpers.calc_iou(R, img_data, C, C.class_mapping)
 
