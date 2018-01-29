3e07c44cb0dc5c0816ebd91cfad1c50a0ca7ea2c
Phu Pham <anphu.1225@gmail.com>
2018-01-29 22:47:07 +0200

    fixed inconsistency between tab vs space indentation

--- test_frcnn.py
+++ test_frcnn.py
@@ -229,12 +229,12 @@ for idx, img_name in enumerate(sorted(os.listdir(img_path))):
     for key in bboxes:
         bbox = np.array(bboxes[key])
 
-		new_boxes, new_probs = roi_helpers.non_max_suppression_fast(bbox, np.array(probs[key]), overlap_thresh=0.5)
-		print('all predictions',new_probs)
-		print('number of detections',len(new_boxes))
+        new_boxes, new_probs = roi_helpers.non_max_suppression_fast(bbox, np.array(probs[key]), overlap_thresh=0.5)
+        print('all predictions', new_probs)
+        print('number of detections', len(new_boxes))
 
-		for jk in range(new_boxes.shape[0]):
-			(x1, y1, x2, y2) = new_boxes[jk,:]
+        for jk in range(new_boxes.shape[0]):
+            (x1, y1, x2, y2) = new_boxes[jk, :]
 
             (real_x1, real_y1, real_x2, real_y2) = get_real_coordinates(ratio, x1, y1, x2, y2)
 
